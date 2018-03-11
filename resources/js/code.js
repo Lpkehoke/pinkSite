7defc690e118ad9199aaeacb71b793b59c38c047
alex <ibymif2000@yandex.ru>
2018-03-11 21:35:27 +0300

    home

---
+++ resources/js/code.js
@@ -0,0 +1,5 @@
+$('.main-part-slider').slick({
+	dots: true,
+	prevArrow: '<div class="__slider-but"><div class="__slider-last"><div></div></div></div>',
+	nextArrow: '<div class="__slider-but"><div class="__slider-next"><div></div></div></div>'
+});

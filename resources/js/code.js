43edd30b3c702ee2516798959cfd36ba97907c3a
alex <ibymif2000@yandex.ru>
2018-03-17 17:37:31 +0300

    some

--- resources/js/code.js
+++ resources/js/code.js
@@ -1,5 +1,9 @@
+'use strict';
+
 $('.main-part-slider').slick({
 	dots: true,
 	prevArrow: '<div class="__slider-but"><div class="__slider-last"><div></div></div></div>',
 	nextArrow: '<div class="__slider-but"><div class="__slider-next"><div></div></div></div>'
 });
+
+$('._slider_').slider({});
